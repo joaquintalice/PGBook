6921aa1f38360a729629b82cfb388731e0417466
Joaquin Talice <joaquintalice@gmail.com>
2023-05-26 21:54:23 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Console is a class which have a (dot "." operator or member access operator) and WriteLine is the method. 
+
+Console.WriteLine("What kind of thing are we talking about?");
+string kindOfThing = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string descriptionOfThing = Console.ReadLine();
+string c = "of Doom";
+string d = "3000";
+Console.WriteLine("The " + kindOfThing + " " + descriptionOfThing + /*" of "*/ " " + c + " " + d + "!");
