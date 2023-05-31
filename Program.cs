92d30f58c26eab07cec6b229f04adee4497de3ce
Joaquin Talice <joaquintalice@gmail.com>
2023-05-31 18:23:23 -0300

    Watchtower

--- Program.cs
+++ Program.cs
@@ -1,13 +1,46 @@
-﻿Console.Title = "Repairing the Clocktower";
-Console.Write("Number: ");
-string input = Console.ReadLine();
-int number = Convert.ToInt32(input);
+﻿Console.Title = "Watchtower";
 
-if (number % 2 == 0)
+Console.Write("Cords X: ");
+string inputX = Console.ReadLine();
+Console.Write("Cords Y: ");
+string inputY = Console.ReadLine();
+int cordX = Convert.ToInt32(inputX);
+int cordY = Convert.ToInt32(inputY);
+
+if (cordX < 0 && cordY > 0)
 {
-    Console.WriteLine("Tick");
+    Console.WriteLine("The enemy is in the NorthWest!");
+}
+else if (cordX == 0 && cordY > 0)
+{
+    Console.WriteLine("The enemy is in the North!");
+}
+else if (cordX > 0 && cordY > 0)
+{
+    Console.WriteLine("The enemy is in the NorthEast!");
+}
+else if (cordX < 0 && cordY == 0)
+{
+    Console.WriteLine("The enemy is in the West!");
+}
+else if (cordX > 0 && cordY == 0)
+{
+    Console.WriteLine("The enemy is in the East!");
+}
+else if (cordX < 0 && cordY < 0)
+{
+    Console.WriteLine("The enemy is in the SouthWest!");
+}
+else if (cordX == 0 && cordY < 0)
+{
+    Console.WriteLine("The enemy is in the South!");
+}
+else if (cordX > 0 && cordY < 0)
+{
+    Console.WriteLine("The enemy is in the SouthEast!");
 }
 else
 {
-    Console.WriteLine("Tock");
+    Console.WriteLine("The enemy is here!");
+    Console.Beep();
 }
