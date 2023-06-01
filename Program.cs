f79b09a5658ddc7ced3215daa9a20428b4344401
Joaquin Talice <joaquintalice@gmail.com>
2023-05-31 21:55:53 -0300

    Buying Inventory and Discounted

--- Program.cs
+++ Program.cs
@@ -1,46 +1,103 @@
-﻿Console.Title = "Watchtower";
+﻿Console.Title = "Buying inventory";
 
-Console.Write("Cords X: ");
-string inputX = Console.ReadLine();
-Console.Write("Cords Y: ");
-string inputY = Console.ReadLine();
-int cordX = Convert.ToInt32(inputX);
-int cordY = Convert.ToInt32(inputY);
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("The following items are available:");
+Console.BackgroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("1 - Rope");
+Console.WriteLine("2 - Torches");
+Console.WriteLine("3 - Climbing Equipment");
+Console.WriteLine("4 - Clean Water");
+Console.WriteLine("5 - Machete");
+Console.WriteLine("6 - Canoe");
+Console.WriteLine("7 - Food Supplies");
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.Write("What number do you want to see the price of?: ");
+Console.BackgroundColor = ConsoleColor.Red;
+int itemID = Convert.ToInt32(Console.ReadLine());
+Console.Write("What's your name?: ");
+string name = Console.ReadLine();
+Console.BackgroundColor = ConsoleColor.Black;
 
-if (cordX < 0 && cordY > 0)
+switch (itemID)
 {
-    Console.WriteLine("The enemy is in the NorthWest!");
-}
-else if (cordX == 0 && cordY > 0)
-{
-    Console.WriteLine("The enemy is in the North!");
-}
-else if (cordX > 0 && cordY > 0)
-{
-    Console.WriteLine("The enemy is in the NorthEast!");
-}
-else if (cordX < 0 && cordY == 0)
-{
-    Console.WriteLine("The enemy is in the West!");
-}
-else if (cordX > 0 && cordY == 0)
-{
-    Console.WriteLine("The enemy is in the East!");
-}
-else if (cordX < 0 && cordY < 0)
-{
-    Console.WriteLine("The enemy is in the SouthWest!");
-}
-else if (cordX == 0 && cordY < 0)
-{
-    Console.WriteLine("The enemy is in the South!");
-}
-else if (cordX > 0 && cordY < 0)
-{
-    Console.WriteLine("The enemy is in the SouthEast!");
-}
-else
-{
-    Console.WriteLine("The enemy is here!");
-    Console.Beep();
+    case 1:
+        if (name == "Joaquin")
+        {
+            Console.WriteLine("Rope cost 5 gold");
+        }
+        else
+        {
+            Console.WriteLine("Rope cost 10 gold");
+        }
+        break;
+
+    case 2:
+        if (name == "Joaquin")
+        {
+            Console.WriteLine("Torches cost 8 gold");
+        }
+        else
+        {
+            Console.WriteLine("Torches cost 16 gold");
+        }
+        break;
+
+    case 3:
+        if (name == "Joaquin")
+        {
+            Console.WriteLine("Climbing Equipment cost 12 gold");
+        }
+        else
+        {
+            Console.WriteLine("Climbing Equipment cost 24 gold");
+        }
+        break;
+
+    case 4:
+        if (name == "Joaquin")
+        {
+            Console.WriteLine("Clean Water cost 1 gold");
+        }
+        else
+        {
+            Console.WriteLine("Clean Water cost 2 gold");
+        }
+        break;
+
+    case 5:
+        if (name == "Joaquin")
+        {
+            Console.WriteLine("Machete cost 10 gold");
+        }
+        else
+        {
+            Console.WriteLine("Machete cost 20 gold");
+        }
+        break;
+
+    case 6:
+        if (name == "Joaquin")
+        {
+            Console.WriteLine("Canoe cost 100 gold");
+        }
+        else
+        {
+            Console.WriteLine("Canoe cost 200 gold");
+        }
+        break;
+
+    case 7:
+        if (name == "Joaquin")
+        {
+            Console.WriteLine("Food Supplies cost 1 gold");
+        }
+        else
+        {
+            Console.WriteLine("Food Supplies cost 2 gold");
+        }
+        break;
+
+    default:
+        Console.WriteLine("This item isn't available");
+        break;
 }
