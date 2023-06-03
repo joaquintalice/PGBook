19508860e0a199c0711b593ae040b0f9f5e87098
Joaquin Talice <joaquintalice@gmail.com>
2023-06-03 19:34:56 -0300

    The Laws of Freach

--- Program.cs
+++ Program.cs
@@ -1,27 +1,12 @@
-﻿Console.Title = "The Replicator of D’To";
-Console.BackgroundColor = ConsoleColor.DarkCyan;
-Console.ForegroundColor = ConsoleColor.Black;
-int[] arr = new int[5];
-Console.WriteLine("Choose five numbers to fill the array");
-for (int i = 0; i < arr.Length; i++)
+﻿Console.Title = "The Laws of Freach";
+
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int total = 0;
+
+foreach (int num in array)
 {
-    Console.Write("Number: ");
-    int numbersFromUser = Convert.ToInt32(Console.ReadLine());
-    arr[i] = numbersFromUser;
-    Console.WriteLine("*******************");
-    Console.WriteLine($"{arr[i]} was added successfully");
-    Console.WriteLine("*******************");
+    total += num;
 }
 
-Console.BackgroundColor = ConsoleColor.Green;
-
-int[] secondArr = new int[5];
-for (int i = 0; i < secondArr.Length; i++)
-{
-    secondArr[i] = arr[i];
-    Console.WriteLine("***************");
-    Console.WriteLine($"{secondArr[i]} was copied successfully");
-    Console.WriteLine("***************");
-}
-Console.BackgroundColor = ConsoleColor.Black;
-Console.ForegroundColor = ConsoleColor.White;
+float average = (float)total / array.Length;
+Console.WriteLine(average);
