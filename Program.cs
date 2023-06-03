ffb52997bebdd82f238d7ca17322f83edc740843
Joaquin Talice <joaquintalice@gmail.com>
2023-06-03 04:22:04 -0300

    The Replicatorr of D'To

--- Program.cs
+++ Program.cs
@@ -1,36 +1,27 @@
-﻿Console.Title = "The Magic Cannon";
-
-Console.Write("What number do you want to the length of the array?: ");
-int agesLength = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("*******************");
-int[] ages = new int[agesLength];
-
-for (int i = 0; i < ages.Length; i++)
+﻿Console.Title = "The Replicator of D’To";
+Console.BackgroundColor = ConsoleColor.DarkCyan;
+Console.ForegroundColor = ConsoleColor.Black;
+int[] arr = new int[5];
+Console.WriteLine("Choose five numbers to fill the array");
+for (int i = 0; i < arr.Length; i++)
 {
-    Console.Write("What number do you want to store in the array?: ");
-    int num = Convert.ToInt32(Console.ReadLine());
-    ages[i] = num;
-    Console.WriteLine($"{num} was stored successfully in the array");
-}
-Console.WriteLine("*******************");
-Console.WriteLine("Inverted ages final result: ");
-
-for (int i = 0; i < ages.Length ;i++)
-{
-    Console.Write($"{ages[^(i+1)]}, ");
+    Console.Write("Number: ");
+    int numbersFromUser = Convert.ToInt32(Console.ReadLine());
+    arr[i] = numbersFromUser;
+    Console.WriteLine("*******************");
+    Console.WriteLine($"{arr[i]} was added successfully");
+    Console.WriteLine("*******************");
 }
 
-Console.WriteLine("");
-Console.WriteLine("*******************");
-int[] range = ages[0..3];
-Console.WriteLine("Range 0 to 3");
-for (int i = 0; i < range.Length; i++)
+Console.BackgroundColor = ConsoleColor.Green;
+
+int[] secondArr = new int[5];
+for (int i = 0; i < secondArr.Length; i++)
 {
-    Console.WriteLine($"{range[i]}");
+    secondArr[i] = arr[i];
+    Console.WriteLine("***************");
+    Console.WriteLine($"{secondArr[i]} was copied successfully");
+    Console.WriteLine("***************");
 }
-Console.WriteLine("*******************");
-Console.WriteLine("Ages range reverted");
-for (int i = 0; i < range.Length; i++)
-{
-    Console.WriteLine($"{range[^(i+1)]}");
-}
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
