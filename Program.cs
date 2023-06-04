403ab5be9af18775b002385475f17119b17521e7
Joaquin Talice <joaquintalice@gmail.com>
2023-06-04 05:46:32 -0300

    Methods Taking a Number 2.0

--- Program.cs
+++ Program.cs
@@ -1,26 +1,59 @@
 ﻿
 int AskForNumber(string text)
 {
-    Console.WriteLine(text);
+    Console.Write(text);
     int result = Convert.ToInt32(Console.ReadLine());
     return result;
 }
 
-AskForNumber("What's the max seed of a Tesla Plaid?");
+int agesLength = AskForNumber("What number do you want to the length of the array?");
+Console.WriteLine("*******************");
+int[] ages = new int[agesLength];
 
-int AskForNumberInRange(string text, int min, int
-    max)
+for (int i = 0; i < ages.Length; i++)
 {
-    Console.WriteLine(text);
-    int number = Convert.ToInt32(Console.ReadLine());
-    while (number < min || number > max)
-    {
-        Console.WriteLine(text);
-        number = Convert.ToInt32(Console.ReadLine());
-    }
-    return number;
+    int num = AskForNumber("What number do you want to store in the array?: ");
+    Console.WriteLine("*******************");
+    ages[i] = num;
+    Console.WriteLine($"{num} was stored successfully in the array");
+    Console.WriteLine("*******************");
+}
+Console.WriteLine("*******************");
+Console.WriteLine("Inverted ages final result: ");
+
+for (int i = 0; i < ages.Length; i++)
+
+{
+
+    Console.Write($"{ages[^(i + 1)]}, ");
+
 }
 
-AskForNumberInRange("Write a number between 0 and 30", 0, 30);
 
+Console.WriteLine("");
 
+Console.WriteLine("*******************");
+
+int[] range = ages[0..3];
+
+Console.WriteLine("Range 0 to 3");
+
+for (int i = 0; i < range.Length; i++)
+
+{
+
+    Console.WriteLine($"{range[i]}");
+
+}
+
+Console.WriteLine("*******************");
+
+Console.WriteLine("Ages range reverted");
+
+for (int i = 0; i < range.Length; i++)
+
+{
+
+    Console.WriteLine($"{range[^(i + 1)]}");
+
+}
