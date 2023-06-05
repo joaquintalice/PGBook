6950f79187986e93394702e015eba5a2644876fb
Joaquin Talice <joaquintalice@gmail.com>
2023-06-05 06:57:45 -0300

    test

--- Program.cs
+++ Program.cs
@@ -4,34 +4,13 @@ int[,] matriz = new int[3, 10];
 
 for (int i = 0; i < matriz.GetLength(0); i++)
 {
-    for (int j = 0; j < matriz.GetLength(1); j++)
-    {
-        Console.Write($"Ingrese el valor para la matriz [{i}, {j}]: ");
-        int value = Convert.ToInt32(Console.ReadLine());
-        matriz[i, j] = value;
-        
-    }
-    Console.WriteLine();
+    for (int current = 1; current <= numberToCountTo; current++)
+        Console.WriteLine(current);
 }
-
-for (int i = 0; i < matriz.GetLength(0); i++)
+int ReadNumber()
 {
-    for (int j = 0; j < matriz.GetLength(1); j++)
-    {
-        Console.Write(matriz[i, j] + " ");
-    }
-    Console.WriteLine();
-}
+    string input = Console.ReadLine();
+    int number = Convert.ToInt32(input);
+    return number;
+};
 
-int[] result = new int[10];
-for (int i = 0; i < matriz.GetLength(0); i++)
-{
-    result[i] = 0;
-    for (int j = 0; j < matriz.GetLength(1); j++)
-    {
-        result[i] += matriz[i, j];
-    }
-    Console.Write($"Suma de la fila {i}: ");
-    Console.Write(result[i] + " ");
-    Console.WriteLine();
-}
