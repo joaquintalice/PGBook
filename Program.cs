0698645657626f08449f351fee70e16eaf75dc97
Joaquin Talice <joaquintalice@gmail.com>
2023-06-10 03:46:33 -0300

    Simula's Soup

--- Program.cs
+++ Program.cs
@@ -1,39 +1,114 @@
-BoxState state = BoxState.Locked;
 
-while (true)
+Console.WriteLine($"Which type of food do you want? Choose a num of the menu.");
+Console.WriteLine($"1: {FoodType.Soup}");
+Console.WriteLine($"2: {FoodType.Stew}");
+Console.WriteLine($"3: {FoodType.Gumbo}");
+Console.Write("Your choose: ");
+int orderNum = Convert.ToInt32(Console.ReadLine());
+FoodType typeOrder;
+while (orderNum < 1 || orderNum > 3)
 {
-    Console.Write($"The chest is {state}. What do you want to do? ");
-    string usrAnswer = Console.ReadLine();
+    Console.Write($"This type isn't in the list. Choose another: ");
+    orderNum = Convert.ToInt32(Console.ReadLine());
+}
 
-    switch (usrAnswer)
-    {
-        case "close":
-            state = BoxState.Unlocked;
-            break;
-        case "lock":
-            state = BoxState.Locked;
-            break;
+if (orderNum == 1)
+{
+    typeOrder = FoodType.Soup;
+}
+else if (orderNum == 2)
+{
+    typeOrder = FoodType.Stew;
+}
+else
+{
+    typeOrder = FoodType.Gumbo;
+}
 
-        case "unlock":
-            state = BoxState.Unlocked;
-            break;
+Console.WriteLine($"Which ingredient do you want to your {typeOrder}? Choose a num from the menu");
+Console.WriteLine($"1: {MainIngredient.Mushrooms}");
+Console.WriteLine($"2: {MainIngredient.Chicken}");
+Console.WriteLine($"3: {MainIngredient.Carrots}");
+Console.WriteLine($"4: {MainIngredient.Potatoes}");
+Console.Write("Your choose: ");
+int ingredientNum = Convert.ToInt32(Console.ReadLine());
+MainIngredient ingredientOrder;
 
-        case "open":
-            state = BoxState.Open;
-            break;
-    }
+while (ingredientNum < 1 || ingredientNum > 4)
+{
+    Console.Write($"This ingredient isn't in the list. Choose another: ");
+    ingredientNum = Convert.ToInt32(Console.ReadLine());
+}
+
+if (ingredientNum == 1)
+{
+    ingredientOrder = MainIngredient.Mushrooms;
+}
+else if (ingredientNum == 2)
+{
+    ingredientOrder = MainIngredient.Chicken;
+}
+else if (ingredientNum == 3)
+{
+    ingredientOrder = MainIngredient.Carrots;
+}
+else
+{
+    ingredientOrder = MainIngredient.Potatoes;
 }
 
 
+Console.WriteLine($"Which type of seasoning do you want? Choose a num of the menu.");
+Console.WriteLine($"1: {Seasoning.Spicy}");
+Console.WriteLine($"2: {Seasoning.Salty}");
+Console.WriteLine($"3: {Seasoning.Sweet}");
+Console.Write("Your choose: ");
+int seasoningNum = Convert.ToInt32(Console.ReadLine());
+Seasoning seasoningOrder;
 
-
-
-
-
-enum BoxState
+while (seasoningNum < 1 || seasoningNum > 3)
 {
-    Locked, 
-    Unlocked,
-    Open,
-    
+    Console.Write($"This type of seasoning isn't in the list. Choose another: ");
+    seasoningNum = Convert.ToInt32(Console.ReadLine());
+}
+
+if (seasoningNum == 1)
+{
+    seasoningOrder = Seasoning.Spicy;
+}
+else if (seasoningNum == 2)
+{
+    seasoningOrder = Seasoning.Salty;
+}
+else
+{
+    seasoningOrder = Seasoning.Sweet;
+}
+
+(Seasoning, MainIngredient, FoodType) Soup = (seasoningOrder, ingredientOrder, typeOrder);
+
+Console.WriteLine($"Your {Soup} is ready. Enjoy it!");
+
+
+
+enum FoodType
+{
+    Soup,
+    Stew,
+    Gumbo
+};
+
+enum MainIngredient
+{
+    Mushrooms,
+    Chicken,
+    Carrots,
+    Potatoes
+}
+
+enum Seasoning
+{
+    Spicy,
+    Salty,
+    Sweet
 }
