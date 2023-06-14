657182f361f9283a79fe9fd9d1e2b4124b7830b0
Joaquin Talice <joaquintalice@gmail.com>
2023-06-14 03:43:45 -0300

    Arrow class with properties

--- Program.cs
+++ Program.cs
@@ -1,33 +1,99 @@
 
-Rectangle rectangle = new Rectangle(2, 3);
-rectangle.SetWidth(5);
-rectangle.SetHeight(5);
-Console.WriteLine(rectangle.GetArea());
+Arrow arrow = getArrow();
+Console.WriteLine($"The total cost of your arrow is {arrow.Cost}");
 
-
-class Rectangle
+Arrow getArrow()
 {
-    private float _width;
-    private float _height;
-    private float _area;
+    Arrowhead arrowhead = GetArrowType();
+    Fletching fletching = GetFletchingType();
+    float length = GetLength();
 
-    public Rectangle(float width, float height)
-    {
-        _width = width;
-        _height = height;
-        _area = _width * _height;
-    }
-    public float GetWidth() => _width;
-    public float GetHeight() => _height;
-    public float GetArea() => _area;
-    public void SetWidth(float value)
-    {
-        _width = value;
-        _area = _width * _height;
-    }
-    public void SetHeight(float value)
-    {
-        _height = value;
-        _area = _width * _height;
-    }
+    return new Arrow(arrowhead, fletching, length);
 }
+
+
+Fletching GetFletchingType()
+{
+    Console.WriteLine("Which fletching do you want?");
+    Console.WriteLine("(plastic, turkey feathers, goose feathers)");
+    string input = Console.ReadLine();
+    return input switch
+    {
+        "plastic" => Fletching.Plastic,
+        "turkey feathers" => Fletching.TurkeyFeathers,
+        "goose feathers" => Fletching.GooseFeathers
+    };
+}
+
+Arrowhead GetArrowType()
+{
+    Console.WriteLine("Which arrowhead do you want?");
+    Console.WriteLine("(steel, wood, obsidian)");
+    string input = Console.ReadLine();
+
+    return input switch
+    {
+        "steel" => Arrowhead.Steel,
+        "wood" => Arrowhead.Wood,
+        "obsidian" => Arrowhead.Obsidian
+    };
+}
+
+
+
+float GetLength()
+{
+    float length = 0;
+
+    while (length < 60 && length > 100)
+    {
+        Console.WriteLine("Which length do you want? Choose between 60 and 100");
+        length = Convert.ToSingle(Console.ReadLine());
+    }
+    return length;
+}
+
+class Arrow
+{
+    private Arrowhead Arrowhead { get; }
+    private Fletching Fletching { get; }
+    private float Length { get; }
+
+    public Arrow(Arrowhead arrowhead, Fletching fletching, float length)
+    {
+        Arrowhead = arrowhead;
+        Fletching = fletching;
+        Length = length;
+    }
+
+    public float Cost
+    {
+        get
+        {
+            float arrowheadCost = Arrowhead switch
+            {
+                Arrowhead.Steel => 10,
+                Arrowhead.Wood => 3,
+                Arrowhead.Obsidian => 5
+            };
+
+            float fletchingCost = Fletching switch
+            {
+                Fletching.Plastic => 10,
+                Fletching.TurkeyFeathers => 5,
+                Fletching.GooseFeathers => 3
+            };
+
+            float shaftCost = 0.05f * Length;
+
+            return arrowheadCost + fletchingCost + shaftCost;
+        }
+    }
+
+
+}
+
+
+
+enum Arrowhead { Steel, Wood, Obsidian }
+enum Fletching { Plastic, TurkeyFeathers, GooseFeathers }
